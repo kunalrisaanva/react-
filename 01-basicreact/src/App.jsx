f38c9b42362e38333ea663b1235ddd2957e965fe
kunal <kunalrisaanva12@gmail.com>
2024-06-18 14:46:12 +0530

    react folder structure

---
+++ 01-basicreact/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+     <h3> hello world | from react </h3>
+  )
+}
+
+export default App
