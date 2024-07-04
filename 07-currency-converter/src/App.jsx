612aa543ee53dc5ea872ee2d79f0fecdc728ddc3
kunal <kunalrisaanva12@gmail.com>
2024-07-04 15:55:37 +0530

    customs hooks

---
+++ 07-currency-converter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [, ] = useState(0)
+
+  return (
+    <>
+    <h1 className=' text-4xl bg-red-400'> Currency exchanger </h1>
+    </>
+  )
+}
+
+export default App
