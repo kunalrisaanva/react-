8e78701fb4500543ac07233c093487ab2957baf6
kunal <kunalrisaanva12@gmail.com>
2024-08-12 17:39:46 +0530

    update heroimage

--- nike-peoject/components/HeroSections.jsx
+++ nike-peoject/components/HeroSections.jsx
@@ -29,7 +29,7 @@ function HeroSections() {
     </div>
 
     <div className="hero-image">
-    <img src="../src/assets/shoe_image.png" alt="hero" />
+    <img src="https://firebasestorage.googleapis.com/v0/b/fir-ef23f.appspot.com/o/shoe_image.png?alt=media&token=5f2070cf-0b14-4a56-a824-660083a25138" alt="hero" />
       
     </div>
    </main>
